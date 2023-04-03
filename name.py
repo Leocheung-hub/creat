7ffb2f030842437981d605635057698cc3dc23e2
Leocheung-hub <129719949+Leocheung-hub@users.noreply.github.com>
2023-04-03 14:09:11 +0800

    Create name.py

---
+++ name.py
@@ -0,0 +1,12 @@
+y_string = "my name is chaitanya baweja"
+
+
+# using the title() function of string class
+new_string = my_string.title()
+
+
+print(new_string)
+
+
+# Output
+# My Name Is Chaitanya Baweja
